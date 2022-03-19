d4ba7f0f4afe65fd69b3c24d5d7f97469a6eade8
Teo <teowenlong0316@gmail.com>
2022-03-19 19:53:47 +0800

    add: swagger docs & note modules

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { SkipJWT } from 'src/guard.decorator';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-
+@ApiTags('auth')
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,9 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('api/v1')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
